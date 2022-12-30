a48b1944d0325ac6edaa6e4168096cb4b575ab75
Tamilselvan0103 <tamilselvans0103@gmail.com>
2022-12-30 23:09:05 +0530

    updated js

--- script.js
+++ script.js
@@ -1,65 +1,59 @@
-var list1 =[];
-var list2 =[];
-var list3 =[];
-var list4 =[];
-var list5 =[];
-var list6 =[];
-var list7 =[];
-var list8 =[];
-let arr =[];
-var n = -1;
-var x = 1;
-var checkedValue=0;
+var row = 1;
 
-button = document.querySelector("#submit");
-button.addEventListener("click",function(){
-    var tableBody = document.querySelector("#table-body");
-    var newRow= tableBody.insertRow(n);
-    list1[x] = document.getElementById("first-name").value;
-    list2[x] = document.querySelector("#last-name").value;
-    list3[x] = document.querySelector("#address").value;
-    list4[x] = document.querySelector("#State").value;
-    list5[x] = document.querySelector("#Country").value;
-    list6[x] = document.querySelector("#pincode").value;
-    list7[x] = document.querySelector("input[name='gender']:checked").value;
- 
-    let list = document.querySelectorAll("li");
-    for(let i=0; i<list.length; i++){
-        let selectedValue= list[i].firstChild;
-        if(selectedValue.checked){
-            checkedValue++;
-           list8 = selectedValue.value;
-           arr.push(list8);
+var btn = document.getElementById("submit")
+
+btn.addEventListener("click", displayElement)
+
+
+function displayElement(event){
+    event.preventDefault();
+    var name = document.getElementById("first-name").value;
+    var lastname = document.getElementById("last-name").value;
+    var address = document.getElementById("address").value;
+    var State = document.getElementById("State").value;
+    var Country = document.getElementById("Country").value;
+    var pincode = document.getElementById("pincode").value;
+    var gender = document.querySelector(`input[name="gender"]:checked`).value;
+    var foodList = [];
+    var list = document.getElementsByName(`flist`)
+    for(var i =0; i<list.length; i++){
+        if(list[i].checked){
+            foodList.push(list[i].value);
         }
     }
 
-    if(checkedValue<2){
-        alert("Please select atleast Two choice of food");
-        arr=[];
-        !important;
-     }
-   
-   var cell1 = newRow.insertCell(n);
-   cell1.innerText = list1[x];
-   var cell2 = newRow.insertCell(n);
-   cell2.innerText = list2[x];
-   var cell3 = newRow.insertCell(n);
-   cell3.innerText = list3[x];
-   var cell4 = newRow.insertCell(n);
-   cell4.innerText = list4[x];
-   var cell5 = newRow.insertCell(n);
-   cell5.innerText = list5[x];
-   var cell6 = newRow.insertCell(n);
-   cell6.innerText = list6[x];
-   var cell7 = newRow.insertCell(n);
-   cell7.innerText = list7[x];
-   var cell8 = newRow.insertCell(n);
-   cell8.innerText= arr;
+    if(foodList.length<2)
+    {
+        alert("please select two food items")
+        return;
+    }
+
+    if(!name || !lastname || !address || !State || !Country || !pincode ||!gender || !foodList)
+    {
+        alert("please fill all the required fields")
+        return;
+    }
+   var table = document.getElementById(`table`)
+   var newrow = table.insertRow(row)
+   var cell1 = newrow.insertCell(0);
+   cell1.textContent = name;
+   var cell2 = newrow.insertCell(1);
+   cell2.textContent = lastname
+   var cell3 = newrow.insertCell(2);
+   cell3.textContent = address
+   var cell4 = newrow.insertCell(3);
+   cell4.textContent = State
+   var cell5 = newrow.insertCell(4);
+   cell5.textContent = Country
+   var cell6 = newrow.insertCell(5);
+   cell6.textContent = pincode;
+   var cell7 = newrow.insertCell(6);
+   cell7.textContent = gender
+   var cell8 = newrow.insertCell(7);
+   cell8.textContent = foodList
+   row++;
    let form = document.getElementById("form")
    form.reset();
-   arr=[];
-   checkedValue=0;
-   list7[x]=[];
-  
-});
 
+
+}
